da8ea191534021cce7704121ca7469025e70feef
BINDU MADHAV <76785889+Bindumadhav1507@users.noreply.github.com>
2024-01-30 22:11:10 +0530

    views.py

--- auth/views.py
+++ auth/views.py
@@ -42,36 +42,45 @@ def getdata(request):
         return JsonResponse(result)
     return JsonResponse({'error': 'Invalid request method'})
 
-
 def content(request, category, option_name):
     assets = category.upper()
     company_name = option_name.capitalize()
-    
+
     page_content = {
         "FI" : {
-                'Capula' : '''Capula Is a town in Michoacan whose name means "The Capulines" are located in the town of Morelia, Mexico and is known for its ceramics.
+                'Capula' : '''Capula Investment Management LLP is a British hedge fund, the fourth largest in Europe, with assets under management (AUM) of about  
+$23 billion as of H2 2020. The firm manages absolute return, enhanced fixed income, macro and crisis alpha strategies.''',
 
-                            After the conquest of Mexico, Vasco de quiroga gathered the Purépecha Indians into several villages around Lake Pátzcuaro, and encouraged each to adopt a specific craft as a means to survival. His vision lives on years later: Capula for its rust-colored pottery.[1]
+                'Pimco' : '''PIMCO (officially Pacific Investment Management Company LLC) is an American investment management firm focusing on active fixed incomm
+e management worldwide. PIMCO manages investments in many asset classes such as fixed income, equities, commodities, asset allocation, ETFs, hedge funds, and privv
+ate equity. PIMCO is one of the largest investment managers, actively managing more than $2 trillion in assets for central banks, sovereign wealth funds, pension  
+funds, corporations, foundations and endowments, and individual investors around the world. PIMCO’s headquarters are in Newport Beach, California; the firm has ovv
+er 3,100 employees working in 22 offices throughout the Americas, Europe, and Asia.PIMCO and Allianz Global Investors manage around €2.5 trillion of third-party aa
+ssets.''',
 
-                            During the autumn in this village held an annual festival called The Calacas of Capula which holds and exhibition of Capula artists[2]"''',
-                            
-                'Pimco' : '''PIMCO (officially Pacific Investment Management Company LLC) is an American investment management firm focusing on active fixed income management worldwide. PIMCO manages investments in many asset classes such as fixed income, equities, commodities, asset allocation, ETFs, hedge funds, and private equity. PIMCO is one of the largest investment managers, actively managing more than $2 trillion in assets for central banks, sovereign wealth funds, pension funds, corporations, foundations and endowments, and individual investors around the world. PIMCO’s headquarters are in Newport Beach, California; the firm has over 3,100 employees working in 22 offices throughout the Americas, Europe, and Asia.PIMCO and Allianz Global Investors manage around €2.5 trillion of third-party assets.''',
-                'Invesco' : '''Invesco Ltd. is an American independent investment management company that is headquartered in Atlanta, Georgia, with additional branch offices in 20 countries. Its common stock is a constituent of the S&P 500 and trades on the New York stock exchange.Invesco operates under the Invesco, Trimark, Invesco Perpetual, WL Ross & Co and Powershares brand names."''',
+                
+                'Invesco' : '''Invesco Ltd. is an American independent investment management company that is headquartered in Atlanta, Georgia, with additional brr
+anch offices in 20 countries. Its common stock is a constituent of the S&P 500 and trades on the New York stock exchange.Invesco operates under the Invesco, Trimaa
+rk, Invesco Perpetual, WL Ross & Co and Powershares brand names.''',
 
               },
-    
+
         "EQ" : {
-                'Wellington' : '''Wellington Management Company is a private, independent investment management firm with client assets under management totaling over US$1 trillion based in Boston, Massachusetts, United States.
+                'Wellington' : '''Wellington Management Company is a private, independent investment management firm with client assets under management totaling  
+over US$1 trillion based in Boston, Massachusetts, United States.
 
-                                  The firm serves as an investment advisor to over 2,200 institutions in over 60 countries, as of 30 June 2020. Its clients include central banks and sovereign institutions, pension funds, endowments and foundations, family offices, fund sponsors, insurance companies, financial intermediaries, and wealth managers.''',
-                            
-                'Henderson' : '''Henderson Group plc was a global investment management company with its principal place of business in the City of London. It merged with Janus Capital Group in May 2017 to create Janus Henderson.
-                                 The Company was established in 1934 to administer the estates of Alexander Henderson, 1st Baron Faringdon.In 1975, it started managing pension funds. It was first listed on the London Stock Exchange in 1983.''',
+                                  The firm serves as an investment advisor to over 2,200 institutions in over 60 countries, as of 30 June 2020. Its clients includd
+e central banks and sovereign institutions, pension funds, endowments and foundations, family offices, fund sponsors, insurance companies, financial intermediariee
+s, and wealth managers.''',
+
+                'Henderson' : '''Henderson Group plc was a global investment management company with its principal place of business in the City of London. It merr
+ged with Janus Capital Group in May 2017 to create Janus Henderson.
+                                 The Company was established in 1934 to administer the estates of Alexander Henderson, 1st Baron Faringdon.In 1975, it started mann
+aging pension funds. It was first listed on the London Stock Exchange in 1983.''',
               },
-    
-    }
+        }
     # Construct the SQL query
-    query = f"SELECT fund_manager, funding, domicile, initial_trade, swaps_included, cleared, bilateral FROM fundmanagersdata WHERE fund_manager = '{company_name}' AND asset_class = '{assets}';"
+    query = f"SELECT fund_manager, funding, domicile, initial_trade, swaps_included, cleared, bilateral FROM FundManagersData WHERE fund_manager = '{company_name}' AND asset_class = '{assets}';"
 
     # Execute the query
     with connection.cursor() as cursor:
@@ -82,7 +91,7 @@ def content(request, category, option_name):
     data = data if data else {'result': None}
 
     # Retrieve distinct product types
-    query = f"SELECT DISTINCT(product_type) FROM fundmanagersdata WHERE fund_manager = '{company_name}' AND asset_class = '{assets}';"
+    query = f"SELECT DISTINCT(product_type) FROM FundManagersData WHERE fund_manager = '{company_name}' AND asset_class = '{assets}';"
     with connection.cursor() as cursor:
         cursor.execute(query)
         products = cursor.fetchall() or "no products"
